cbd2db441645bcc0a14ba292865a4206b2b0af1e
Anusha <anushadhulipudi.ad@gmail.com>
2018-07-26 10:22:08 +0530

    Stack Practice

--- src/stacksAndQueues/Problem2_3.java
+++ src/stacksAndQueues/Problem2_3.java
@@ -4,45 +4,82 @@ import java.util.Stack;
 
 public class Problem2_3 {
 	
-	Stack<Integer> stack;
-	Stack<Integer> minStack;
+	Stack<Integer> stack = new Stack<Integer>();
+	Stack<Integer> minStack = new Stack<Integer>();
 	
 	public void push(int element) {
-		if(stack.isEmpty()) {
+		if(stack.isEmpty() == true) {
 			stack.push(element);
 			minStack.push(element);
 		} else {
 			stack.push(element);
 			int min = minStack.pop();
+			minStack.push(min);
 			if(min > element) {
 				minStack.push(element);
 			} else {
 				minStack.push(min);
 			}
 		}
+		
 	}
 	
 	public int pop() {
-		//if(!stack.isEmpty()) {
+		if(!stack.isEmpty()) {
 			int element = stack.pop();
-			minStack.pop();
+			int min = minStack.pop();
+			
 			return element;
-		//} 
+		} else {
+			System.out.println("Stack is empty!");
+			return 0; 
+		}
+		
 	}
 	
-	public int findMin() {
-		//if(!stack.isEmpty()) {
+	public void findMin() {
+		if(!stack.isEmpty()) {
 			int element = minStack.pop();
 			minStack.push(element);
-			return element;
-		//} 
+			System.out.println("Min element:: " + element);
+		} else {
+			System.out.println("Stack is empty!");
+		}
+	}
+	
+	public boolean isEmpty() {
+		if(stack.size() > 0) {
+			return true;
+		} 
+		return false;
+	}
+	
+	public void printStack() {
+		System.out.println("Stack:");
+		for(int i : stack) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		
+		System.out.println("Min Stack:");
+		for(int i : minStack) {
+			System.out.print(i + " ");
+		}
+		System.out.println("**********");
 	}
 	
 	public static void main(String args[]) {
 		Problem2_3 myStack = new Problem2_3();
+		//myStack.printStack();
 		myStack.push(100);
-		myStack.push(250);
-		System.out.println("Min::" + myStack.findMin());
+		myStack.findMin();
+		myStack.push(50);
+		myStack.push(2000);
+		myStack.findMin();
+		myStack.pop();
+		myStack.findMin();
+		myStack.pop();
+		myStack.findMin();
 	}
 
 }
